a623044245109d5f7678c5aee720ba37c8609737
Santiago Rodriguez <santiago.rodriguez9@utp.edu.co>
2024-12-08 21:19:42 -0500

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+
+module.exports = {
+    content: ['./src//*.{astro,html,js,jsx,ts,tsx,vue}'],
+    darkMode: 'class', // or 'class'
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
